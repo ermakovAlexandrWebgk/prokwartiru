5ce83d4f51156267cb5732462a1f5526033ce5a5
Admin <gitwebgk@webgk.ru>
2018-05-28 17:18:35 +0300

    add companions to element page

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,273 +1,82 @@
-<?
-/*Version 0.3 2011-04-25*/
-
-/************** OLD ****************/   
-// AddEventHandler("iblock", "OnAfterIBlockElementUpdate", "BXIBlockAfterSave");
-// AddEventHandler("iblock", "OnAfterIBlockElementAdd", "BXIBlockAfterSave");
-// AddEventHandler("catalog", "OnPriceAdd", "BXIBlockAfterSave");
-// AddEventHandler("catalog", "OnPriceUpdate", "BXIBlockAfterSave");
-// function BXIBlockAfterSave($arg1, $arg2 = false)
-// {
-// $ELEMENT_ID = false;
-//     $IBLOCK_ID = false;
-//     $OFFERS_IBLOCK_ID = false;
-//     $OFFERS_PROPERTY_ID = false;
-//
-//     //Check for catalog event
-//     if(is_array($arg2) && $arg2["PRODUCT_ID"] > 0)
-//     {
-//         //Get iblock element
-//         $rsPriceElement = CIBlockElement::GetList(
-//             array(),
-//             array(
-//                 "ID" => $arg2["PRODUCT_ID"],
-//             ),
-//             false,
-//             false,
-//             array("ID", "IBLOCK_ID")
-//         );
-//         if($arPriceElement = $rsPriceElement->Fetch())
-//         {
-//             $arCatalog = CCatalog::GetByID($arPriceElement["IBLOCK_ID"]);
-//             if(is_array($arCatalog))
-//             {
-//                 //Check if it is offers iblock
-//                 if($arCatalog["OFFERS"] == "Y")
-//                 {
-//                     //Find product element
-//                     $rsElement = CIBlockElement::GetProperty(
-//                         $arPriceElement["IBLOCK_ID"],
-//                         $arPriceElement["ID"],
-//                         "sort",
-//                         "asc",
-//                         array("ID" => $arCatalog["SKU_PROPERTY_ID"])
-//                     );
-//                     $arElement = $rsElement->Fetch();
-//                     if($arElement && $arElement["VALUE"] > 0)
-//                     {
-//                         $ELEMENT_ID = $arElement["VALUE"];
-//                         $IBLOCK_ID = $arCatalog["PRODUCT_IBLOCK_ID"];
-//                         $OFFERS_IBLOCK_ID = $arCatalog["IBLOCK_ID"];
-//                         $OFFERS_PROPERTY_ID = $arCatalog["SKU_PROPERTY_ID"];
-//                     }
-//                 }
-//                 //or iblock wich has offers
-//                 elseif($arCatalog["OFFERS_IBLOCK_ID"] > 0)
-//                 {
-//                     $ELEMENT_ID = $arPriceElement["ID"];
-//                     $IBLOCK_ID = $arPriceElement["IBLOCK_ID"];
-//                     $OFFERS_IBLOCK_ID = $arCatalog["OFFERS_IBLOCK_ID"];
-//                     $OFFERS_PROPERTY_ID = $arCatalog["OFFERS_PROPERTY_ID"];
-//                 }
-//                 //or it's regular catalog
-//                 else
-//                 {
-//                     $ELEMENT_ID = $arPriceElement["ID"];
-//                     $IBLOCK_ID = $arPriceElement["IBLOCK_ID"];
-//                     $OFFERS_IBLOCK_ID = false;
-//                     $OFFERS_PROPERTY_ID = false;
-//                 }
-//             }
-//         }
-//     }
-//     //Check for iblock event
-//     elseif(is_array($arg1) && $arg1["ID"] > 0 && $arg1["IBLOCK_ID"] > 0)
-//     {
-//         //Check if iblock has offers
-//         $arOffers = CIBlockPriceTools::GetOffersIBlock($arg1["IBLOCK_ID"]);
-//         if(is_array($arOffers))
-//         {
-//             $ELEMENT_ID = $arg1["ID"];
-//             $IBLOCK_ID = $arg1["IBLOCK_ID"];
-//             $OFFERS_IBLOCK_ID = $arOffers["OFFERS_IBLOCK_ID"];
-//             $OFFERS_PROPERTY_ID = $arOffers["OFFERS_PROPERTY_ID"];
-//         }
-//     }
-//
-//     if($ELEMENT_ID)
-//     {
-//         static $arPropCache = array();
-//         if(!array_key_exists($IBLOCK_ID, $arPropCache))
-//         {
-//             //Check for MINIMAL_PRICE property
-//             $rsProperty = CIBlockProperty::GetByID("MINIMUM_PRICE", $IBLOCK_ID);
-//             $arProperty = $rsProperty->Fetch();
-//             if($arProperty)
-//                 $arPropCache[$IBLOCK_ID] = $arProperty["ID"];
-//             else
-//                 $arPropCache[$IBLOCK_ID] = false;
-//         }
-//
-//         if($arPropCache[$IBLOCK_ID])
-//         {
-//             //Compose elements filter
-//             $arProductID = array($ELEMENT_ID);
-//             if($OFFERS_IBLOCK_ID)
-//             {
-//                 $rsOffers = CIBlockElement::GetList(
-//                     array(),
-//                     array(
-//                         "IBLOCK_ID" => $OFFERS_IBLOCK_ID,
-//                         "PROPERTY_".$OFFERS_PROPERTY_ID => $ELEMENT_ID,
-//                     ),
-//                     false,
-//                     false,
-//                     array("ID")
-//                 );
-//                 while($arOffer = $rsOffers->Fetch())
-//                     $arProductID[] = $arOffer["ID"];
-//             }
-//
-//             $minPrice = false;
-//             $maxPrice = false;
-//             //Get prices
-//             $rsPrices = CPrice::GetList(
-//                 array(),
-//                 array(
-//                     "BASE" => "Y",
-//                     "PRODUCT_ID" => $arProductID,
-//                 )
-//             );
-//             while($arPrice = $rsPrices->Fetch())
-//             {
-//                 $PRICE = $arPrice["PRICE"];
-//
-//                 if($minPrice === false || $minPrice > $PRICE)
-//                     $minPrice = $PRICE;
-//
-//                 if($maxPrice === false || $maxPrice < $PRICE)
-//                     $maxPrice = $PRICE;
-//             }
-//
-//             //Save found minimal price into property
-//             if($minPrice !== false)
-//             {
-//                 CIBlockElement::SetPropertyValuesEx(
-//                     $ELEMENT_ID,
-//                     $IBLOCK_ID,
-//                     array(
-//                         "MINIMUM_PRICE" => $minPrice,
-//                         "MAXIMUM_PRICE" => $maxPrice,
-//                     )
-//                 );
-//             }
-//         }
-//     }
-// }
-?>
-<?
-// Р”РѕР±Р°РІР»РµРЅРёРµ РїСѓРЅРєС‚Р° РјРµРЅСЋ РІ Р°РґРјРёРЅРёСЃС‚СЂР°С‚РёРІРЅСѓСЋ РїР°РЅРµР»СЊ ambu
-// AddEventHandler("main", "OnBuildGlobalMenu", "OnBuildGlobalMenuH");
-//
-// function OnBuildGlobalMenuH(&$aGlobalMenu, &$aModuleMenu)
-// {
-//     foreach ($aModuleMenu as $key => $arMenu)
-//     {
-//      if ($arMenu["parent_menu"] == "global_menu_store")
-//      {
-//       $aModuleMenu[] = array(
-//           "parent_menu" => "global_menu_store",
-//           "section" => "panel_admin",
-//           "sort" => 1000,  // РЎРѕСЂС‚РёСЂРѕРІРєР°
-//           "text" => "РџРµСЂРµРѕС†РµРЅРєР°",  //РќР°Р·РІР°РЅРёРµ РїСѓРЅРєС‚Р° РјРµРЅСЋ
-//           "title" => "РџРµСЂРµРѕС†РµРЅРєР° РІ СЃРѕРѕС‚РІРµС‚СЃС‚РІРёРё СЃ РєСѓСЂСЃРѕРј РµРІСЂРѕ",  // Р’СЃРїР»С‹РІР°СЋС‰Р°СЏ РїРѕРґСЃРєР°Р·РєР°
-//           "icon" => "statistic_icon_sites",
-//           "page_icon" => "statistic_page_sites",
-//           "items_id" => "panel_admin_id",
-//           "url" => "/bitrix/admin/price_index.php?lang=ru", //РџСѓС‚СЊ Рє СЃС‚СЂР°РЅРёС†Рµ
-//           );
-// //      $aModuleMenu[] = array(
-// //          "parent_menu" => "global_menu_store",
-// //          "section" => "panel_admin",
-// //          "sort" => 1100,  // РЎРѕСЂС‚РёСЂРѕРІРєР°
-// //          "text" => "Р’С‹РіСЂСѓР·РєР° РІ РЇРЅРґРµРєСЃ",  //РќР°Р·РІР°РЅРёРµ РїСѓРЅРєС‚Р° РјРµРЅСЋ
-// //          "title" => "Р’С‹РіСЂСѓР·РєР° РєР°С‚Р°Р»РѕРіР° РЅР° РЇРЅРґРµРєСЃ РњР°СЂРєРµС‚",  // Р’СЃРїР»С‹РІР°СЋС‰Р°СЏ РїРѕРґСЃРєР°Р·РєР°
-// //          "icon" => "statistic_icon_sites",
-// //          "page_icon" => "statistic_page_sites",
-// //          "items_id" => "panel_admin_id",
-// //          "url" => "/bitrix/admin/yandex_index.php?lang=ru", //РџСѓС‚СЊ Рє СЃС‚СЂР°РЅРёС†Рµ
-// //          );
-//       break;
-//      }
-//
-//     }
-// }
-
+<? 
 /************** NEW ****************/
 
-if (file_exists($_SERVER['DOCUMENT_ROOT']."/local/php_interface/config.php")) {
-    include_once($_SERVER['DOCUMENT_ROOT']."/local/php_interface/config.php");
-}
-
-AddEventHandler("currency", "CurrencyFormat", "myFormat");
-function myFormat($fSum, $strCurrency)
-{
-    if ($strCurrency == "RUB") {
-        return number_format ( $fSum, 0, '.', ' ' ).' р';
+    if (file_exists($_SERVER['DOCUMENT_ROOT']."/local/php_interface/config.php")) {
+        include_once($_SERVER['DOCUMENT_ROOT']."/local/php_interface/config.php");
     }
-}
 
-function arshow($array, $adminCheck = false){
-    global $USER;
-    $USER = new Cuser;
-    if ($adminCheck) {
-        if (!$USER->IsAdmin()) {
-            return false;
+    AddEventHandler("currency", "CurrencyFormat", "myFormat");
+    function myFormat($fSum, $strCurrency)
+    {
+        if ($strCurrency == "RUB") {
+            return number_format ( $fSum, 0, '.', ' ' ).' р';
         }
     }
-    echo "<pre>";
-    // var_dump($array);
-    print_r($array);
-    echo "</pre>";
-}
 
-//Р’С‹Р±РѕСЂРєР° С‡РїСѓ-СЃСЃС‹Р»РѕРє РёР· РјРѕРґСѓР»СЏ sotbit seo РїРѕ id СЂР°Р·РґРµР»Р°
-function GetSeoLinks($sectionId)
-{
-    GLOBAL $DB;
-    $arSeoLinks = array();
-    $dbResult = $DB->Query("SELECT * FROM `b_sotbit_seometa_chpu` WHERE `ACTIVE` = 'Y' AND `section_id`=".intval($sectionId));
-    while ($row = $dbResult->Fetch())
-    {
-        $row["PROPERTIES"] = unserialize($row["PROPERTIES"]);
-        $dbSubResults = $DB->Query("SELECT `NAME` FROM `b_sotbit_seometa` WHERE `ID`=".$row["CONDITION_ID"])->Fetch();
-        $row["CONDITION"] = $dbSubResults["NAME"];
-        $arSeoLinks[] = $row;
+    function arshow($array, $adminCheck = false){
+        global $USER;
+        $USER = new Cuser;
+        if ($adminCheck) {
+            if (!$USER->IsAdmin()) {
+                return false;
+            }
+        }
+        echo "<pre>";
+        // var_dump($array);
+        print_r($array);
+        echo "</pre>";
     }
-    return $arSeoLinks;
-}
-/*
-$arSeoLn = GetSeoLinks($arResult["VARIABLES"]["SECTION_ID"]);
 
-foreach($arSeoLn as $seoLn)
-{
-    $arSeoLinks[$seoLn["NAME"]] = $seoLn["NEW_URL"];
+    //Выборка чпу-ссылок из модуля sotbit seo по id раздела
+    function GetSeoLinks($sectionId)
+    {
+        GLOBAL $DB;
+        $arSeoLinks = array();
+        $dbResult = $DB->Query("SELECT * FROM `b_sotbit_seometa_chpu` WHERE `ACTIVE` = 'Y' AND `section_id`=".intval($sectionId));
+        while ($row = $dbResult->Fetch())
+        {
+            $row["PROPERTIES"] = unserialize($row["PROPERTIES"]);
+            $dbSubResults = $DB->Query("SELECT `NAME` FROM `b_sotbit_seometa` WHERE `ID`=".$row["CONDITION_ID"])->Fetch();
+            $row["CONDITION"] = $dbSubResults["NAME"];
+            $arSeoLinks[] = $row;
+        }
+        return $arSeoLinks;
+    }
 
-    // if(strpos($seoLn["CONDITION"], "/РїСЂРёРІСЏР·РєР°") !== FALSE && $seoLn["REAL_URL"] == $APPLICATION->GetCurPage(false))
-    // {
-    //     $arSeoLinks = false;
-    //     break;
-    // }
-    // if(strpos($seoLn["CONDITION"], "/Р±СЂРµРЅРґ") !== FALSE && $seoLn["REAL_URL"] == $APPLICATION->GetCurPage(false))
-    // {
-    //     $vendor = $seoLn["NEW_URL"];
-    //     $vendor_code = Cutil::translit($seoLn["PROPERTIES"]["PROIZVODITEL"][0], "ru");
-    //     $arSeoLinks = false;
-    //     break;
-    // }
-    // if(strpos($seoLn["CONDITION"], "/РІРµСЂС…") !== FALSE && $seoLn["REAL_URL"] != $APPLICATION->GetCurPage(false))
-    // {
-    //     $arSeoLinks[$seoLn["NAME"]] = $seoLn["NEW_URL"];
-    // }
-}
-?>
+    /**
+    * метод для получения компаньонов
+    * 
+    * @param integer $ID - ID элемента каталога, для котрого ищем компаньонов
+    */
+    function GetCompanions($ID) {
 
-<div class="often_seek">
-<span>Р§Р°СЃС‚Рѕ РёС‰СѓС‚:</span>
-<?foreach($arSeoLinks as $name => $link):?>
-<a href="<?=$link?>"><?=$name?></a>
-<?endforeach?>
-</div>
+        $ID = intval($ID);
+        if (empty($ID)) { 
+            return false;
+        }
 
-*/         
+        $items = array();
+        $result = false;
+
+        //ищем элемент инфоблока "компаньоны", в котором текущий товар связан с другими
+        $companionBlock = CIBlockElement::GetList(array(), array("IBLOCK_CODE" => "companions", "PROPERTY_ITEMS" => $ID), false, false, array("ID", "PROPERTY_ITEMS", "IBLOCK_ID"));
+        while($arCompanion = $companionBlock->Fetch()) {
+
+            //получаем значение свойство "Компаньоны" и собираем ID товаров-компаньонов
+            $itemsCompanions = CIBlockElement::GetProperty($arCompanion["IBLOCK_ID"], $arCompanion["ID"], array(), array("CODE" => "ITEMS"));
+            while($arItem = $itemsCompanions->Fetch()) {
+                //добавляем в конечный результат все товары, кроме текущего
+                if ($arItem["VALUE"] != $ID) {
+                    $items[$arItem["VALUE"]] = $arItem["VALUE"];  
+                }  
+            } 
+        }
+
+        if (count($items) > 0) {
+            $result = $items;     
+        }
+
+        return $result;
+
+    }       
 ?>

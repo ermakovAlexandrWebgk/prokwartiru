e205c756bb273b8d80c6c5f3912956a9df7ae35f
Admin <gitwebgk@webgk.ru>
2018-08-13 16:36:44 +0300

    SUP-8269: switch analog and companion slider

--- local/templates/aspro_next/components/bitrix/catalog.element/main/template.php
+++ local/templates/aspro_next/components/bitrix/catalog.element/main/template.php
@@ -1317,83 +1317,7 @@ $p1 = "/catalog/oboi/";
 if (strstr($APPLICATION->GetCurDir(), $p1)) {?>
 <div class="slider-area">
 
-    <?
-    if (!empty($arResult['PROPERTIES']["COLOR"]["VALUE"]) && (!empty($arResult['PROPERTIES']["DESIGN_OBOI"]["VALUE"]))){
-        $GLOBALS['filterColor'] = array(
-            "PROPERTY_COLOR_VALUE"      =>$arResult['PROPERTIES']["COLOR"]["VALUE"],
-            "PROPERTY_DESIGN_OBOI_VALUE"=>$arResult['PROPERTIES']["DESIGN_OBOI"]["VALUE"]
-        );
-
-    ?>
-    <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"slider2",
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => "slider2",
-		"DETAIL_URL" => "/catalog/#SECTION_CODE_PATH#/#ELEMENT_CODE#/",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "DETAIL_PICTURE",
-			1 => "",
-		),
-		"FILTER_NAME" => "filterColor",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "77",
-		"IBLOCK_TYPE" => "catalog",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "30",
-		"NORMAL_BLOCK" => "Y",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "34639",
-		"PARENT_SECTION_CODE" => "oboi",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "",
-			1 => "PRICE",
-			2 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"SHOW_DETAIL_LINK" => "Y",
-		"SORT_BY1" => "NAME",
-		"SORT_BY2" => "RANDOM",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "RANDOM",
-		"STRICT_SECTION_CHECK" => "N"
-	),
-	false
-);
-}?>
+    
  <?
     $companions = GetCompanions($arResult["ID"]);
     $GLOBALS['filterCompanion'] = array(
@@ -1469,6 +1393,83 @@ if (strstr($APPLICATION->GetCurDir(), $p1)) {?>
 );?>
     <?}?>
 </div>
+<?
+    if (!empty($arResult['PROPERTIES']["COLOR"]["VALUE"]) && (!empty($arResult['PROPERTIES']["DESIGN_OBOI"]["VALUE"]))){
+        $GLOBALS['filterColor'] = array(
+            "PROPERTY_COLOR_VALUE"      =>$arResult['PROPERTIES']["COLOR"]["VALUE"],
+            "PROPERTY_DESIGN_OBOI_VALUE"=>$arResult['PROPERTIES']["DESIGN_OBOI"]["VALUE"]
+        );
+
+    ?>
+    <?$APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "slider2",
+    array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "COMPONENT_TEMPLATE" => "slider2",
+        "DETAIL_URL" => "/catalog/#SECTION_CODE_PATH#/#ELEMENT_CODE#/",
+        "DISPLAY_BOTTOM_PAGER" => "N",
+        "DISPLAY_DATE" => "Y",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => "Y",
+        "DISPLAY_PREVIEW_TEXT" => "Y",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => array(
+            0 => "DETAIL_PICTURE",
+            1 => "",
+        ),
+        "FILTER_NAME" => "filterColor",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => "77",
+        "IBLOCK_TYPE" => "catalog",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "INCLUDE_SUBSECTIONS" => "Y",
+        "MESSAGE_404" => "",
+        "NEWS_COUNT" => "30",
+        "NORMAL_BLOCK" => "Y",
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Новости",
+        "PARENT_SECTION" => "34639",
+        "PARENT_SECTION_CODE" => "oboi",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => array(
+            0 => "",
+            1 => "PRICE",
+            2 => "",
+        ),
+        "SET_BROWSER_TITLE" => "N",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SHOW_404" => "N",
+        "SHOW_DETAIL_LINK" => "Y",
+        "SORT_BY1" => "NAME",
+        "SORT_BY2" => "RANDOM",
+        "SORT_ORDER1" => "DESC",
+        "SORT_ORDER2" => "RANDOM",
+        "STRICT_SECTION_CHECK" => "N"
+    ),
+    false
+);
+}?>
 <?}?>
 <?
 global $arTheme, $arRegion;
